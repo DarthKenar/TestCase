b4b04cc33acb491ef4977e87f10e9d08617c467f
Federico HP <federico_vega22@hotmail.com>
2022-11-05 18:34:41 -0300

    initialized Test Cases whit Pytest

---
+++ PyTest/tests/test_main.py
@@ -0,0 +1,22 @@
+from package.main import Solution
+
+def test_string():
+    assert Solution.upperNextToLower("ahjJa") == [2,3]
+    assert Solution.upperNextToLower("aasdDknpqn") == [3,4]
+    assert Solution.upperNextToLower("aAsdDknpqn") == [0,1]
+    assert Solution.upperNextToLower("aasdkKJnpqn") == [4,5]
+    assert Solution.upperNextToLower("aasdaknpqnN") == [9,10]
+    assert Solution.upperNextToLower("apsS") == [2,3]
+    assert Solution.upperNextToLower("AasdDknpqn") == [0,1]
+
+    assert Solution.upperNextToLower("aasdgknpqn")
+    assert Solution.upperNextToLower("")
+    assert Solution.upperNextToLower("E")
+
+
+
+
+
+
+
+
